82d3491f2e402c2567d33688ef25b6c43aadb0bf
QUZESHENG <50943636+QUZESHENG@users.noreply.github.com>
2020-05-17 10:30:40 +0800

    上传
    是

---
+++ P40.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main()
+{
+	int n_int = INT_MAX;
+	cout << "int is " <<sizeof(int)<<endl;
+	cout << "n_int "<< n_int <<endl;
+	int wren(432);
+	cout << "wren = " << wren<<endl;
+}
